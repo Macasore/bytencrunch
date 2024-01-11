806aebf803c828e476b9b2a4a94d6e6938b1e909
boredoreo <134864203+boredoreo@users.noreply.github.com>
2024-01-11 17:28:20 +0100

    Add files via upload

---
+++ ByteNCrunch/handlers/admin.py
@@ -0,0 +1,38 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import  CallbackQueryHandler, CommandHandler
+import csv, os
+
+from database.query import fetch_todays_fluter_orders, fetch_todays_orders
+
+
+def fetch_from_direct_transfer(update, bot):
+    my_orders = fetch_todays_orders()
+    user_id = update.effective_user.id
+    with open(f'{user_id}.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(my_orders)
+
+    bot.bot.send_document(update.effective_chat.id,  open(f'{user_id}.csv', 'rb'))
+    os.remove(f'{user_id}.csv')
+
+def fetch_from_flutter(update, bot):
+    my_orders = fetch_todays_fluter_orders()
+    user_id = update.effective_user.id
+    with open(f'{user_id}_flutter.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(my_orders)
+
+    bot.bot.send_document(update.effective_chat.id,  open(f'{user_id}_flutter.csv', 'rb'))
+    os.remove(f'{user_id}_flutter.csv')
+
+
+fetch_from_direct_transfer_handler = CommandHandler(
+    "direct_transfer",
+    fetch_from_direct_transfer
+)
+
+fetch_from_flutter_handler = CommandHandler(
+    "flutter",
+    fetch_from_flutter
+)
+    
