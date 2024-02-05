94612848d9ba808dd903d247ce93637833554d6a
boredoreo <134864203+boredoreo@users.noreply.github.com>
2024-02-05 04:44:10 +0100

    Update checkout.py

--- ByteNCrunch/handlers/checkout.py
+++ ByteNCrunch/handlers/checkout.py
@@ -68,7 +68,7 @@ def checkout(update, bot):
 def direct_transfer(update, bot):
     query = update.callback_query
     
-    time_limit_upper = 6
+    time_limit_upper = 2
     time_limit_lower = 16
     current_time = datetime.now().strftime("%Y-%m-%d, %H:%M").split(",")[1].split(":")[0].strip()
     ct_int = int(current_time)
